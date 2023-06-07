a2ce48af9fc7ffa778a60511ad9e04d8cbc1dd9c
Syafrillah Najjar <132876332+syfrillhnjjr@users.noreply.github.com>
2023-06-07 14:08:06 +0700

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,42 @@
+google.charts.load('current', { packages: ['corechart'] });
+google.charts.setOnLoadCallback(getSpreadsheetData);
+
+function getSpreadsheetData() {
+    var spreadsheetId = '1kh865J7uGpI_FglAOGmOFKGGZatCn6Cc5oi-0l_0vgE';
+    var range = 'TS!A1:C13';
+
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/' + spreadsheetId + '/gviz/tq?gid=0&range=' + range);
+    query.send(handleQueryResponse);
+}
+
+function handleQueryResponse(response) {
+    if (response.isError()) {
+        console.error('Error: ' + response.getMessage());
+        return;
+    }
+
+    var data = response.getDataTable();
+    drawCharts(data);
+}
+
+function drawCharts(data) {
+    var options = {
+        title: 'Data Penjualan Furniture, Office Supplies, dan Technology pada Negara Bagian',
+        width: 400,
+        height: 300
+    };
+
+    var chart1 = new google.visualization.ColumnChart(document.getElementById('chart1'));
+    chart1.draw(data, options);
+
+    var chart2 = new google.visualization.PieChart(document.getElementById('chart2'));
+    chart2.draw(data, options);
+
+    var chart3 = new google.visualization.BubbleChart(document.getElementById('chart3'));
+    chart3.draw(data, options);
+
+    var chart4 = new google.visualization.AreaChart(document.getElementById('chart4'));
+    chart4.draw(data, options);
+
+    // Tambahkan jenis grafik lain dan elemen div untuk grafik lainnya
+}
